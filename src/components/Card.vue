fafb87ac44a3fbb11afbfcebbd5e8ae1e16c4ca9
kasule joseph <joseph.kasule@andela.com>
2019-11-10 15:58:43 +0200

    add image card

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <img :src="collection.imageUrl" :alt="collection.author">
+            <h4>{{collection.author}}</h4>
+            <p>{{collection.description}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['collection'],
+    name: 'card'
+}
+</script>
